6e5f2818085739c4c53a72010223b44b3d6ddb94
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-25 13:38:11 -0600

    UI better

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -1,9 +1,48 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const Item = () => {
+const Item = ({ item }) => {
+	const { ref, inView } = useInView({
+		triggerOnce: true, // Trigger animation only once
+		threshold: 0.1, // 10% of the element needs to be visible
+	});
+
+	const variants = {
+		hidden: { opacity: 0, x: 50 },
+		visible: (i) => ({
+			opacity: 1,
+			x: 0,
+			transition: {
+				delay: i * 0.1,
+			},
+		}),
+	};
+
 	return (
-		<div className="bg-neutral-100 border-2 border-neutral-200/25 p-2 rounded-md">
-			<h2 className="text-neutral-700">Hello</h2>
+		<div
+			ref={ref}
+			className="bg-neutral-100 border-2 border-neutral-200/25 p-2 rounded-md"
+		>
+			<h2 className="text-neutral-900">
+				{item.name}{" "}
+				<span className="text-neutral-500">{`(${item.amount})`}</span>
+			</h2>
+			<div className="flex gap-2 my-2">
+				{["Store 1", "Store 2", "Store 3"].map((store, index) => (
+					<motion.span
+						key={index}
+						className="py-1 px-3 bg-blue-200 rounded-full text-xs text-neutral-800"
+						custom={index}
+						initial="hidden"
+						animate={inView ? "visible" : "hidden"}
+						variants={variants}
+					>
+						{store}
+					</motion.span>
+				))}
+			</div>
 		</div>
 	);
 };
--- app/components/List.jsx
+++ app/components/List.jsx
@@ -2,7 +2,25 @@
 import { motion } from "framer-motion";
 import Item from "./Item";
 
-const items = new Array(8).fill(null); // Create an array with 8 items
+const items = [];
+/* {
+    name: "Apples",
+    amount: "4",
+    store: "Walmart",
+    notes: "",
+},
+{
+    name: "Tea",
+    amount: "24oz",
+    store: "Winco",
+    notes: "Mango tea and Nut tea",
+},
+{
+    name: "Spagetti",
+    amount: "1 pack",
+    store: "Winco",
+    notes: "",
+}, */
 
 const List = () => {
 	return (
@@ -11,18 +29,24 @@ const List = () => {
 				<h1 className="text-neutral-900 w-fit text-[2rem] jura mx-auto my-4">
 					Grocery List
 				</h1>
-				<div className="flex flex-col gap-2 p-2">
-					{items.map((_, index) => (
-						<motion.div
-							key={index}
-							initial={{ opacity: 0, y: -25 }}
-							animate={{ opacity: 1, y: 0 }}
-							transition={{ delay: index * 0.1 }}
-						>
-							<Item key={index} index={index} />
-						</motion.div>
-					))}
-				</div>
+				{items.length < 1 ? (
+					<div>
+						<h1 className="text-neutral-900">No items yet</h1>
+					</div>
+				) : (
+					<div className="flex flex-col gap-2 p-2">
+						{items.map((item, index) => (
+							<motion.div
+								key={index}
+								initial={{ opacity: 0, y: -25 }}
+								animate={{ opacity: 1, y: 0 }}
+								transition={{ delay: index * 0.1 }}
+							>
+								<Item key={index} index={index} item={item} />
+							</motion.div>
+						))}
+					</div>
+				)}
 			</div>
 		</div>
 	);
