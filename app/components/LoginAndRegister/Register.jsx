d554d3d0f4f312b339e7c3dd2623ccf12e872434
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-25 21:51:22 -0600

    prisma

--- app/components/LoginAndRegister/Register.jsx
+++ app/components/LoginAndRegister/Register.jsx
@@ -1,30 +1,70 @@
+"use client";
+import React, { useState } from "react";
 import Input from "@/app/components/Input";
-import React from "react";
 
 const Register = () => {
+	const [data, setData] = useState({
+		username: "",
+		email: "",
+		password: "",
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setData({
+			...data,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(data);
+	};
+
 	return (
-		<div>
-			<div className="min-h-screen bg-indigo-400 flex items-center justify-center">
-				<div className="w-[94vw] bg-white rounded-lg px-4">
-					<h1 className="text-neutral-900 w-fit text-[2rem] jura mx-auto my-4 mb-8">
-						Register
-					</h1>
-					<div className="flex flex-col gap-2">
-						<Input label={"Username"} />
-						<Input label={"Email"} />
-						<Input label={"Password"} />
-					</div>
-					<button className="w-full py-3 bg-neutral-800 text-white rounded-md my-4 mt-6">
-						Register
-					</button>
-					<p className="text-neutral-500 text-center mb-4 font-light">
-						Already have an account?{" "}
-						<a href="/login" className="text-neutral-800 font-medium">
-							Login
-						</a>
-					</p>
+		<div className="min-h-screen bg-indigo-400 flex items-center justify-center">
+			<form
+				className="w-[94vw] bg-white rounded-lg px-4"
+				onSubmit={handleSubmit}
+			>
+				<h1 className="text-neutral-900 w-fit text-[2rem] jura mx-auto my-4 mb-8">
+					Register
+				</h1>
+				<div className="flex flex-col gap-2">
+					<Input
+						label={"Username"}
+						name="username"
+						value={data.username}
+						onChange={handleChange}
+					/>
+					<Input
+						label={"Email"}
+						name="email"
+						value={data.email}
+						onChange={handleChange}
+					/>
+					<Input
+						label={"Password"}
+						type="password"
+						name="password"
+						value={data.password}
+						onChange={handleChange}
+					/>
 				</div>
-			</div>
+				<button
+					type="submit"
+					className="w-full py-3 bg-neutral-800 text-white rounded-md my-4 mt-6"
+				>
+					Register
+				</button>
+				<p className="text-neutral-500 text-center mb-4 font-light">
+					Already have an account?{" "}
+					<a href="/login" className="text-neutral-800 font-medium">
+						Login
+					</a>
+				</p>
+			</form>
 		</div>
 	);
 };
